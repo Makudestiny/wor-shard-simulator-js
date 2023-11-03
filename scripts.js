26f639eff1391baff19db7c8a924ed19c8c41641
Adam Wise <wise.adamj@gmail.com>
2023-11-02 22:41:11 -0500

    Initial Script file and css updates

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+const rareCrystalSummonChances = {
+    uncommon:.5000,
+    rare:.3751,
+    rareLord:.0399,
+    epic:.0788,
+    epicLord:.0012,
+    legendary: .0046,
+    legendaryLord: .0004
+}
+
+function simulateCrystalPull(type) {
+    let randValue = Math.random();
+
+}
